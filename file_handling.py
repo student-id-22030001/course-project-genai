fcd5fdd3b81d7375463c4144cbe0496941d50c70
student-id-22030001 <163560836+student-id-22030001@users.noreply.github.com>
2024-05-05 02:05:00 +0500

    handled exception for invalid URL

--- file_handling.py
+++ file_handling.py
@@ -3,6 +3,7 @@ import streamlit as st
 from pytube import YouTube
 from chat_management import add_message
 from transcription import transcribe_audio
+from pytube.exceptions import RegexMatchError
 from moviepy.editor import VideoFileClip, AudioFileClip
 
 def save_uploaded_file(uploaded_file):
@@ -40,16 +41,21 @@ def extract_youtube_audio(video_file_path):
     return audio_file_path
 
 def download_youtube_video(youtube_url):
-    yt = YouTube(youtube_url)
-    video = yt.streams.filter(only_audio=True).first()
-    video_file_path = 'temp_video.mp4'
-    video.download(filename=video_file_path)
-    return video_file_path
+    try:
+        yt = YouTube(youtube_url)
+        video = yt.streams.filter(only_audio=True).first()
+        video_file_path = 'temp_video.mp4'
+        video.download(filename=video_file_path)
+        return video_file_path
+    except RegexMatchError as e:
+        st.error("Please enter a valid YouTube URL.")
 
 def process_youtube_url(youtube_url):
     with st.status("Processing URL...", expanded=True) as status:
         st.write("Fetching video from YouTube")
         file_path = download_youtube_video(youtube_url)
+        if file_path is None:
+            return None
         st.write("Converting video to audio")
         audio_file_path = extract_youtube_audio(file_path)
         st.write("Transcribing audio")
